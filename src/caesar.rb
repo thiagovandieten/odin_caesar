7e0845948b00e741149cd7cd1cc8cd334d954cca
Thiago van Dieten <thiago@tvdieten.com>
2024-07-20 00:57:43 +0200

    constant values upper & lowercase
    These are the bit numbers that correlate to the letter

--- src/caesar.rb
+++ src/caesar.rb
@@ -1,3 +1,10 @@
-def caesar_cipher
-  pass
+UC_START = 65
+UC_END = 90
+LC_START = 97
+LC_END = 122
+
+def caesar_cipher(input, offset)
+  return 'Your string is not ASCII compatible for the conversion!' unless input.ascii_only?
+
+  string_in_bytes = input.bytes
 end
