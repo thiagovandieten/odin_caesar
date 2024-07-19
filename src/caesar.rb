7dcbd03440b2c9bdc39f416101bc8a3d6d2e4d24
Thiago van Dieten <thiago@tvdieten.com>
2024-07-20 01:00:32 +0200

    add determine casing method

--- src/caesar.rb
+++ src/caesar.rb
@@ -7,4 +7,19 @@ def caesar_cipher(input, offset)
   return 'Your string is not ASCII compatible for the conversion!' unless input.ascii_only?
 
   string_in_bytes = input.bytes
+  new_bytes = string_in_bytes.map do |chr|
+    # Determine lower_case or higher case
+    is_lowercase = determine_casing(chr)
+  new_bytes.join
+end
+
+def determine_casing(chr)
+  case chr
+  when LC_START..LC_END
+    true
+  when UC_START..UC_END
+    false
+  else
+    'You have entered a incompatible ASCII'
+  end
 end
