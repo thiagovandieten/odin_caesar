b9b18cdfc8b915f105bcc108d868a689a2e8b388
Thiago van Dieten <thiago@tvdieten.com>
2024-07-21 19:00:07 +0200

    fix RSpec typo

--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 require_relative '../src/caesar'
 
-Rspec.describe 'Caesar exercise tests' do
+RSpec.describe 'Caesar exercise tests' do
   describe 'Turn "What a string!" into "Bmfy f xywnsl!"' do
     it 'returns "Bmfy f xywnsl!' do
       expect(caesar_cipher('What a string!', 5)).to eq('Bmfy f xywnsl!')
